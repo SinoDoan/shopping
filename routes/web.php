81220f000925ff1c7344efb5fbf740718f47d7be
Trung Doan <dctrung95@gmail.com>
2021-04-22 22:22:58 +0700

    AdminLogin

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'AdminController@Admin');
+Route::get('/admin', 'AdminController@LoginAdmin');
+Route::post('/admin', 'AdminController@PostLoginAdmin');
+
 Route::get('/home', function () {
     return view('home');
 });
