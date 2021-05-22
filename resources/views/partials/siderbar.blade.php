77bc76738ebae976c0e1b57818d168df81f0b60a
Trung Doan <dctrung95@gmail.com>
2021-05-22 16:27:03 +0700

    sliderFunction

--- resources/views/partials/siderbar.blade.php
+++ resources/views/partials/siderbar.blade.php
@@ -39,7 +39,7 @@
             <a href="{{route('categories.index')}}" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
-                Danh mục sản phẩm 
+                Danh mục sản phẩm
                 <span class="right badge badge-danger">New</span>
               </p>
             </a>
@@ -60,6 +60,14 @@
               </p>
             </a>
           </li>
+            <li class="nav-item">
+                <a href="{{route('slider.index')}}" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                        Slider
+                    </p>
+                </a>
+            </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
