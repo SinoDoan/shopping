c3d0c1748a104ea6704b3a12d23a85fc52036551
Trung Doan <dctrung95@gmail.com>
2021-06-10 14:00:14 +0700

    showFormSearchProduct

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,36 @@
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
-    </ul>
+    <form class="form-inline">
+
+        <div class="form-group mx-sm-2 mb-2">
+            <label class="sr-only"> Nhập ID </label>
+            <input type="text" class="form-control" placeholder="Nhập Id">
+        </div>
+
+        <div class="form-group mx-sm-2 mb-2">
+            <label class="sr-only"> Nhập tên sản phẩm </label>
+            <input type="text" class="form-control" placeholder="Nhập tên sản phẩm">
+        </div>
+
+        <div class="form-group mx-sm-2 mb-2">
+            <label class="sr-only"> Nhập tags sản phẩm </label>
+            <input type="text" class="form-control" placeholder="Nhập tags sản phẩm">
+        </div>
+
+        <div class="form-group mx-sm-2 mb-2">
+            <label class="sr-only"> Nhập tên sản phẩm </label>
+            <select class="form-control">
+                <option value="">Chọn danh mục sản phẩm</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+
+
+        <button type="submit" class="btn btn-primary mb-2">Search</button>
+    </form>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
